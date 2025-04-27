8e13b3c8949775d02ed725ce44a523681067ff55
todolist <chbabmohamed3@gmail.com>
2025-04-27 13:36:15 +0100

    work correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && (action_name == 'new' || action_name == 'create')
+      "auth"
+    else
+      "application"
+    end
+  end
+
 end
